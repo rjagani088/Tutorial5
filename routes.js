b5ce6df2dfb682f39dd97978b545f443eb94ef35
Rahul Jagani <96898001+rjagani088@users.noreply.github.com>
2022-02-03 23:17:35 +0530

    host

---
+++ routes.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var router = express.Router();
+var Movie = require('./Models/Movie')
+var User = require('./Models/User')
+var bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken')
+
+
+
+//to fetch movies
+router.get('/movies',async(req,res)=>{
+    const imovie = await Movie.find()
+    res.send(imovie)
+})
+
+//to add the movies
+router.post("/movies",async(req,res)=>{
+    const imovie = new Movie({
+        name:req.body.name,
+        rating:req.body.rating
+    })
+
+    await imovie.save((err,msg)=>{
+        if(err){
+            res.status(500).json({
+                "error":err
+            })
+        }
+        else{
+            res.status(200).json({
+                "My-message":msg
+            })
+        }
+    })
+
+})
+
+
+// api for updating movie
+
+router.patch('/movies/:id',async (req,res)=>{
+    const imovie = await Movie.findOne({_id:req.params.id})
+    imovie.name = req.body.name
+    imovie.rating = req.body.rating
+    await imovie.save((err,msg)=>{
+        if(err){
+            res.status(500).json({
+                error:err
+            })
+        }
+        else{
+            res.status(200).json({
+                msg:msg
+            })
+        }
+    })
+
+})
+
+//delete api
+
+router.delete("/movies/:name",async(req,res)=>{
+    await Movie.deleteOne({name:req.params.name},(err,msg)=>{
+        if(err){
+            res.status(500).json({
+                error:err
+            })
+        }
+        else{
+            res.status(200).json({
+                msg:msg
+            })
+        }
+
+    })
+})
+
+
+router.post('/users',async(req,res)=>{
+    
+    //generate salt key
+    salt = await bcrypt.genSalt(10)
+    console.log(salt)
+
+    hashedpswd = await bcrypt.hash(req.body.password,salt)
+    console.log(hashedpswd)
+
+    const iuser = new User({
+        uname:req.body.uname,
+        password:hashedpswd
+    })  
+    await iuser.save((err,msg)=>{
+        if(err){
+            res.status(500).json({
+                "error":err
+            })
+        }
+        else{
+            res.status(200).json({
+                "My-message":msg
+            })
+        }
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router 
